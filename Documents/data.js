85f0a2b462b94dd6542415c845a14f1d80e7ab38
jwchao2020 <jwchao2020@users.noreply.github.com>
2025-12-28 08:20:02 +0000

    deploy: 7749c86c058dcb83728962fd717b3f4be6fc8418

--- Documents/data.js
+++ Documents/data.js
@@ -1,4 +1,4 @@
-const LOCAL_DATA = { version: '1766898223', list: [
+const LOCAL_DATA = { version: '1766910001', list: [
   {
     "title": "0.1_四足机器人RL路线",
     "path": "content/0_学习路线&总览/0.1_四足机器人RL路线.html",
@@ -10,7 +10,7 @@ const LOCAL_DATA = { version: '1766898223', list: [
     "folder": "0_学习路线&总览"
   },
   {
-    "title": "⚠️ 0.2_具身智能全栈知识地图 (损坏)",
+    "title": "0.2_具身智能全栈知识地图",
     "path": "content/0_学习路线&总览/0.2_具身智能全栈知识地图.html",
     "folder": "0_学习路线&总览"
   },
