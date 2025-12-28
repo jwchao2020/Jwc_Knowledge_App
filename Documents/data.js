f9811a6b735f0369a63114ce25c6d253f7517154
jwchao2020 <jwchao2020@users.noreply.github.com>
2025-12-28 08:48:21 +0000

    deploy: c8e6e5db7d5fbe14926196e861c1a7f51cf36a94

--- Documents/data.js
+++ Documents/data.js
@@ -1,4 +1,4 @@
-const LOCAL_DATA = { version: '1766910203', list: [
+const LOCAL_DATA = { version: '1766911700', list: [
   {
     "title": "0.1_四足机器人RL路线",
     "path": "content/0_学习路线&总览/0.1_四足机器人RL路线.html",
